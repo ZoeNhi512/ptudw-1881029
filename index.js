7bf004ebf58d37b94fbc88470b095ee3b5c1e6f8
YenNhi Chan <yennhi97tannghia@gmail.com>
2021-08-21 18:12:19 +0700

    finsh-project

--- index.js
+++ index.js
@@ -52,4 +52,5 @@ app.set ('port', process.env.PORT|| 5000);
 app.listen(app.get('port'), () => {
     console.log(`Server is running at port ${app.get('port')}`);
 });
-
+//"nodemon index.js"
+/* */
